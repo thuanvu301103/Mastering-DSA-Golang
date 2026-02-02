e4828c2f4c80ef7837e308cd76f1eb323fc90272
ThuanVu <thuan.vu.e@ctmcorp.com.vn>
2026-02-02 15:19:32 +0700

    test/static_array: Adjust implmented functions

--- phase2_linear/arrays/ds_static_array.go
+++ phase2_linear/arrays/ds_static_array.go
@@ -1,9 +1,7 @@
 package arrays
 
-// StaticArrayOps is a concrete implementation of the ArrayOps interface
 type StaticArrayOps struct{}
 
-// SumArray implements ArrayOps
 func (s StaticArrayOps) SumArray(arr [5]int) int {
 	sum := 0
 	for _, v := range arr {
@@ -12,7 +10,6 @@ func (s StaticArrayOps) SumArray(arr [5]int) int {
 	return sum
 }
 
-// AddMatrices implements ArrayOps
 func (s StaticArrayOps) AddMatrices(a, b [2][2]int) [2][2]int {
 	var result [2][2]int
 	for i := 0; i < 2; i++ {
@@ -23,7 +20,6 @@ func (s StaticArrayOps) AddMatrices(a, b [2][2]int) [2][2]int {
 	return result
 }
 
-// GetCharFrequency implements ArrayOps
 func (s StaticArrayOps) GetCharFrequency(str string) [26]int {
 	var freq [26]int
 	for _, char := range str {
@@ -34,7 +30,6 @@ func (s StaticArrayOps) GetCharFrequency(str string) [26]int {
 	return freq
 }
 
-// UpdateFirstElement implements ArrayOps
 func (s StaticArrayOps) UpdateFirstElement(arr *[3]int, newValue int) {
 	if arr != nil {
 		arr[0] = newValue
